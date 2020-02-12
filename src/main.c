58b5c5e585e8d77b9121f9447c806536e8317256
Elfreda Drowzee <edrowzee@at-l4.21-school.ru>
2020-02-12 16:30:46 +0300

    Norme

--- src/main.c
+++ src/main.c
@@ -27,22 +27,70 @@ static int	ft_path_limit(t_array *arr)
 	return (min);
 }
 
+void		validate_input(t_array *arr, int argc, char **argv)
+{
+	t_input	*input;
+
+	input = NULL;
+	t_input_malloc(&input);
+	ft_reader(argc, argv, input, arr);
+	t_input_print(input);
+	t_input_free(input);
+}
+
+static int	paths_work(t_paths **paths, t_paths **prev, int *path_counter)
+{
+	if (*path_counter && (*paths)->time > (*prev)->time)
+	{
+		t_paths_free(*paths);
+		*paths = *prev;
+		return (0);
+	}
+	else if (*path_counter)
+		t_paths_free(*prev);
+	(*path_counter)++;
+	return (1);
+}
+
+static int	graph_work(t_array *arr, t_paths *paths)
+{
+	int i;
+
+	i = 0;
+	while (i < paths->curr_path)
+	{
+		ft_expand_graph(&arr, paths->path_arr[i]->path,
+				paths->path_arr[i]->size);
+		i++;
+	}
+	paths->path_arr[paths->curr_path] = find_path_bf_new(&arr);
+	if (!paths->path_arr[paths->curr_path])
+		return (0);
+	ft_replace_cpy_in_path(&arr, paths->path_arr[paths->curr_path]);
+	paths->curr_path++;
+	while (merge_paths(arr, paths))
+		;
+	paths->time = ft_calc_path_time(&arr, paths, -1, -1);
+	ft_reset_graph(&arr);
+	return (1);
+}
+
 int			main(int argc, char **argv)
 {
 	t_array	*arr;
 	t_paths	*paths;
 	t_paths	*prev;
-	t_input	*input;
-	int		i;
 	int		path_counter;
 	int		path_limit;
+	t_input	*input; //commit this
 
 	arr = NULL;
-	input = NULL;
 	ft_arr_malloc(&arr);
-	t_input_malloc(&input);
-	ft_reader(argc, argv, input, arr);
-	t_input_print(input);
+	input = NULL; //commit this
+	t_input_malloc(&input); //commit this
+	ft_reader(argc, argv, input, arr); //commit this
+	t_input_print(input); //commit this
+//	validate_input(arr, argc, argv); //uncommit this
 	paths = create_t_paths();
 	path_counter = 0;
 	path_limit = ft_path_limit(arr);
@@ -51,33 +99,13 @@ int			main(int argc, char **argv)
 	{
 		if (path_counter)
 			prev = copy_t_paths(paths);
-		i = 0;
-		while (i < paths->curr_path)
-		{
-			ft_expand_graph(&arr, paths->path_arr[i]->path, paths->path_arr[i]->size);
-			i++;
-		}
-		paths->path_arr[paths->curr_path] = find_path_bf_new(&arr);
-		if (!paths->path_arr[paths->curr_path])
+		if (graph_work(arr, paths) == 0)
 			break ;
-		ft_replace_cpy_in_path(&arr, paths->path_arr[paths->curr_path]);
-		paths->curr_path++;
-		while (merge_paths(arr, paths))
-			;
-		paths->time = ft_calc_path_time(&arr, paths, -1, -1);
-		if (path_counter && paths->time > prev->time)
-		{
-			t_paths_free(paths);
-			paths = prev;
+		if (paths_work(&paths, &prev, &path_counter) == 0)
 			break ;
-		}
-		else if (path_counter)
-			t_paths_free(prev);
-		ft_reset_graph(&arr);
-		path_counter++;
 	}
 	ft_ants_prepare_to_parade(&arr, paths, -1);
-	t_input_free(input);
 	t_paths_free(paths);
+	t_input_free(input); //commit this
 	exit(0);
 }
